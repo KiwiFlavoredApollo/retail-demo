8ed743fa14784a5c161747ccf02b8b1d17602fc0
fractalbarista <fractalbarista@gmail.com>
2026-09-10 16:17:35 +0900

    MainPage 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,105 @@
-import { Button, Container, Stack } from "@chakra-ui/react";
+import {
+  Avatar,
+  Button,
+  type ButtonProps,
+  Card,
+  type CardRootProps,
+  Container,
+  Grid,
+  GridItem, IconButton, Input,
+  InputGroup,
+  Stack,
+  Text,
+  Image, Heading, Box
+} from "@chakra-ui/react";
+import exampleImage from "@/assets/hero.png"
+import { LuBell, LuMail, LuSearch, LuShoppingCart, LuUser } from "react-icons/lu";
 
 export const MainPage = () => {
   return (
     <>
-      <Container maxWidth={"lg"} height={"100vh"}>
-        <Stack direction={"column"}>
-          <Button>Hello World</Button>
-        </Stack>
-      </Container>
+      <Grid templateRows={ "auto" } gap={ "8" }>
+        <GridItem padding={ "2" }>
+          <Container maxWidth={ "8xl" }>
+            <Stack direction={ "row-reverse" } gap={ 4 }>
+              <IconButton rounded={ "full" } variant={ "ghost" }>
+                <LuShoppingCart></LuShoppingCart>
+              </IconButton>
+              <IconButton rounded={ "full" } variant={ "ghost" }>
+                <LuUser></LuUser>
+              </IconButton>
+            </Stack>
+          </Container>
+        </GridItem>
+
+        <GridItem>
+          <Container maxWidth={ "xl" }>
+            <InputGroup width={ "100%" } startElement={ <LuSearch></LuSearch> }>
+              <Input></Input>
+            </InputGroup>
+          </Container>
+        </GridItem>
+
+        <GridItem>
+          <Box height={ "400px" } backgroundColor={ "white" }></Box>
+        </GridItem>
+
+        <GridItem>
+          <Container maxWidth={ "8xl" }>
+            <Grid templateColumns={ "repeat(4, 1fr)" } gap={ "8" }>
+              <GridItem>
+                <Card.Root>
+                  <Card.Header>
+                    <Card.Title>
+                      바람막이
+                    </Card.Title>
+                  </Card.Header>
+                  <Card.Body>
+                    <Image src={ exampleImage }></Image>
+                  </Card.Body>
+                </Card.Root>
+              </GridItem>
+              <GridItem>
+                <Card.Root>
+                  <Card.Header>
+                    <Card.Title>
+                      트레이닝 팬츠
+                    </Card.Title>
+                  </Card.Header>
+                  <Card.Body>
+                    <Image src={ exampleImage }></Image>
+                  </Card.Body>
+                </Card.Root>
+              </GridItem>
+              <GridItem>
+                <Card.Root>
+                  <Card.Header>
+                    <Card.Title>
+                      숏 팬츠
+                    </Card.Title>
+                  </Card.Header>
+                  <Card.Body>
+                    <Image src={ exampleImage }></Image>
+                  </Card.Body>
+                </Card.Root>
+              </GridItem>
+              <GridItem>
+                <Card.Root>
+                  <Card.Header>
+                    <Card.Title>
+                      러닝
+                    </Card.Title>
+                  </Card.Header>
+                  <Card.Body>
+                    <Image src={ exampleImage }></Image>
+                  </Card.Body>
+                </Card.Root>
+              </GridItem>
+            </Grid>
+          </Container>
+        </GridItem>
+      </Grid>
     </>
-  );
+  )
+    ;
 }
