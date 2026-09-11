753fadd9f24ee7828b8ed60758d5c056ad6634d0
fractalbarista <fractalbarista@gmail.com>
2026-09-11 09:13:52 +0900

    DashboardPage 루트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { MainPage } from "@/pages/MainPage.tsx";
+import { DashboardPage } from "@/pages/DashboardPage.tsx";
 import { ShoppingCartPage } from "@/pages/ShoppingCartPage.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path={ "/" } element={ <MainPage></MainPage> }></Route>
+          <Route path={ "/dashboard" } element={ <DashboardPage></DashboardPage> }></Route>
           <Route path={ "/shopping-cart" } element={ <ShoppingCartPage></ShoppingCartPage> }></Route>
         </Routes>
       </BrowserRouter>
